d7db0933ddbace283111556d2add61ea22a8dc80
Nattaya-ati <goodmorningsuncoffee@gmail.com>
2025-05-18 17:06:55 +0700

    all done

--- lib/views/result_screen_ui.dart
+++ lib/views/result_screen_ui.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(ResultScreenUI());
-}
+import 'package:intl/intl.dart';
 
 class ResultScreenUI extends StatefulWidget {
-  const ResultScreenUI({super.key});
+  //สร้างตัวแปรรับค่าที่จะส่งมา
+  double? carPrice;
+  double? payPerMonth;
+  int? month;
+
+  //เอาตัวแปรที่สร้าง  มารอรับค่าที่ส่ง
+  ResultScreenUI({super.key, this.carPrice, this.payPerMonth, this.month});
 
   @override
   State<ResultScreenUI> createState() => _ResultScreenUIState();
@@ -14,6 +17,88 @@ class ResultScreenUI extends StatefulWidget {
 class _ResultScreenUIState extends State<ResultScreenUI> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    //กำหนดรูปแบบของตัวเลข ณ ที่นี้ คอมมาคั่นหลักพัน/ล้าน และทศนิยม 2 ตำแหน่ง
+    NumberFormat fm = NumberFormat("#,###,##0.00");
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text(
+          'CI Calculator',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'ผ่อนทั้งหมด ',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black, // สีข้อความปกติ
+                    ),
+                  ),
+                  TextSpan(
+                    text: '${widget.month}',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red, // สีแดงสำหรับตัวเลข
+                    ),
+                  ),
+                  TextSpan(
+                    text: ' เดือน',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              'ค่างวดรถต่อเดือน',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '${fm.format(widget.payPerMonth)}',
+              style: TextStyle(
+                fontSize: 50.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+            Text(
+              'บาท',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
